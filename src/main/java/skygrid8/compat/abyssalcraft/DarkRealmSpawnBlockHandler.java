c188d4c3ccd6679665f10ca7716abd247ac87d8e
Shinoow <felix@lundquist.ws>
2016-04-27 12:36:11 +0200

    Player relocating
    When entering the Dark Realm, the player will also be moved to the top
    of the grid (along with a block being placed underneath them).

--- src/main/java/skygrid8/compat/abyssalcraft/DarkRealmSpawnBlockHandler.java
+++ src/main/java/skygrid8/compat/abyssalcraft/DarkRealmSpawnBlockHandler.java
@@ -1,5 +1,7 @@
 package skygrid8.compat.abyssalcraft;
 
+import skygrid8.core.SG_Settings;
+
 import com.shinoow.abyssalcraft.AbyssalCraft;
 import com.shinoow.abyssalcraft.api.block.ACBlocks;
 
@@ -11,9 +13,11 @@ public class DarkRealmSpawnBlockHandler {
 	@SubscribeEvent
 	public void onDimensionChange(PlayerChangedDimensionEvent event){
 		if(event.toDim == AbyssalCraft.configDimId4){
-			if(!event.player.worldObj.isRemote)
+			if(!event.player.worldObj.isRemote){
+				event.player.setPositionAndUpdate(event.player.posX, SG_Settings.height - SG_Settings.dist + 1, event.player.posZ);
 				if(event.player.worldObj.isAirBlock(event.player.getPosition().down()))
 					event.player.worldObj.setBlockState(event.player.getPosition().down(), ACBlocks.darkstone.getDefaultState());
+			}
 		}
 	}
 }
