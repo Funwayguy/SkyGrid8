46261195698d634452d4d5ba06ae106784e31829
Shinoow <felix@lundquist.ws>
2017-02-21 21:59:52 +0100

    Fixed a Extra Utilities 2 crash
    Also made the AbyssalCraft Grid World Providers extend their normal
    counterparts

--- src/main/java/funwayguy/skygrid/compat/abyssalcraft/WorldProviderDreadlandsGrid.java
+++ src/main/java/funwayguy/skygrid/compat/abyssalcraft/WorldProviderDreadlandsGrid.java
@@ -1,16 +1,14 @@
 package funwayguy.skygrid.compat.abyssalcraft;
 
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.IChunkGenerator;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+
+import com.shinoow.abyssalcraft.common.world.WorldProviderDreadlands;
 import com.shinoow.abyssalcraft.lib.ACLib;
+
 import funwayguy.skygrid.config.GridRegistry;
 import funwayguy.skygrid.world.ChunkProviderGrid;
 
-public class WorldProviderDreadlandsGrid extends WorldProvider {
+public class WorldProviderDreadlandsGrid extends WorldProviderDreadlands {
 
 	@Override
 	public void createBiomeProvider() {
@@ -20,65 +18,7 @@ public class WorldProviderDreadlandsGrid extends WorldProvider {
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
-	public Vec3d getFogColor(float par1, float par2) {
-		return new Vec3d(0.20000000298023224D, 0.029999999329447746D, 0.029999999329447746D);
-	}
-
-	@Override
-	protected void generateLightBrightnessTable() {
-		float f = 0.35F;
-
-		for (int i = 0; i <= 15; ++i) {
-			float f1 = 1.0F - i / 15.0F;
-			lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * (1.0F - f) + f;
-		}
-	}
-
-	@Override
 	public IChunkGenerator createChunkGenerator() {
 		return new ChunkProviderGrid(worldObj, worldObj.getSeed(), GridRegistry.blocksDreadlands);
-	}
-
-	@Override
-	public boolean isSurfaceWorld() {
-		return false;
-	}
-
-	@Override
-	public boolean canCoordinateBeSpawn(int par1, int par2) {
-		return false;
-	}
-
-	@Override
-	public float calculateCelestialAngle(long par1, float par3) {
-		return 0.5F;
-	}
-
-	@Override
-	public boolean canRespawnHere() {
-		return false;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public boolean doesXZShowFog(int par1, int par2) {
-		return true;
-	}
-
-	@Override
-	public String getSaveFolder() {
-		return "The_Dreadlands";
-	}
-
-	@Override
-	public int getAverageGroundLevel() {
-		return 50;
-	}
-
-	@Override
-	public DimensionType getDimensionType() {
-
-		return ACLib.THE_DREADLANDS;
 	}
 }
--- src/main/java/funwayguy/skygrid/world/WorldProviderNetherGrid.java
+++ src/main/java/funwayguy/skygrid/world/WorldProviderNetherGrid.java
@@ -1,123 +1,15 @@
 package funwayguy.skygrid.world;
 
-import funwayguy.skygrid.config.GridRegistry;
-import net.minecraft.init.Biomes;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.biome.BiomeProviderSingle;
-import net.minecraft.world.border.WorldBorder;
+import net.minecraft.world.WorldProviderHell;
 import net.minecraft.world.chunk.IChunkGenerator;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import funwayguy.skygrid.config.GridRegistry;
 
-public class WorldProviderNetherGrid extends WorldProvider
+public class WorldProviderNetherGrid extends WorldProviderHell
 {
-    /**
-     * creates a new world chunk manager for WorldProvider
-     */
-	@Override
-    public void createBiomeProvider()
-    {
-        this.biomeProvider = new BiomeProviderSingle(Biomes.HELL);
-        this.isHellWorld = true;
-        this.hasNoSky = true;
-    }
 
-    /**
-     * Return Vec3D with biome specific fog color
-     */
-    @SideOnly(Side.CLIENT)
-    public Vec3d getFogColor(float p_76562_1_, float p_76562_2_)
-    {
-        return new Vec3d(0.20000000298023224D, 0.029999999329447746D, 0.029999999329447746D);
-    }
-
-    /**
-     * Creates the light to brightness table
-     */
-    protected void generateLightBrightnessTable()
-    {
-        float f = 0.1F;
-
-        for (int i = 0; i <= 15; ++i)
-        {
-            float f1 = 1.0F - (float)i / 15.0F;
-            this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * (1.0F - f) + f;
-        }
-    }
-    
     @Override
     public IChunkGenerator createChunkGenerator()
     {
         return new ChunkProviderGrid(this.worldObj, this.worldObj.getSeed(), GridRegistry.blocksNether);
-    }
-
-    /**
-     * Returns 'true' if in the "main surface world", but 'false' if in the Nether or End dimensions.
-     */
-    @Override
-    public boolean isSurfaceWorld()
-    {
-        return false;
-    }
-
-    /**
-     * Will check if the x, z position specified is alright to be set as the map spawn point
-     */
-    @Override
-    public boolean canCoordinateBeSpawn(int x, int z)
-    {
-        return false;
-    }
-
-    /**
-     * Calculates the angle of sun and moon in the sky relative to a specified time (usually worldTime)
-     */
-    @Override
-    public float calculateCelestialAngle(long worldTime, float partialTicks)
-    {
-        return 0.5F;
-    }
-
-    /**
-     * True if the player can respawn in this dimension (true = overworld, false = nether).
-     */
-    @Override
-    public boolean canRespawnHere()
-    {
-        return false;
-    }
-
-    /**
-     * Returns true if the given X,Z coordinate should show environmental fog.
-     */
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean doesXZShowFog(int x, int z)
-    {
-        return true;
-    }
-    
-    @Override
-    public WorldBorder createWorldBorder()
-    {
-        return new WorldBorder()
-        {
-            public double getCenterX()
-            {
-                return super.getCenterX() / 8.0D;
-            }
-            public double getCenterZ()
-            {
-                return super.getCenterZ() / 8.0D;
-            }
-        };
-    }
-    
-    @Override
-    public DimensionType getDimensionType()
-    {
-        return DimensionType.NETHER;
     }
 }
